570bbaa531d9a03db6890dbc0274fefc7ffe02db
Ribeiro <joao.ribeiro17@outlook.com>
2023-03-21 18:12:13 -0300

    Criação do primeiro projeto Js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var mensagem = document.querySelector('div#msg')
+    var foto = document.querySelector('img#imagem')
+    var hor = new Date()
+    var min = new Date()
+    var hora = hor.getHours()
+    var minutos = min.getMinutes()
+    mensagem.innerHTML = `<p>Agora são <strong>${hora} Horas</strong> e <strong>${minutos} min.</strong></p>`
+    if (hora >= 00 && hora < 12) {
+        foto.src = 'imagens/manha.png'
+        document.body.style.background = '#FBE88D'
+    } else if (hora >= 12 && hora <= 18) {
+        foto.src = 'tarde.png'
+        document.body.style.background = '#fc6703'
+    } else {
+        foto.src = 'imagens/noite.png'
+        document.body.style.background = '#192528'
+        
+    }
+}
