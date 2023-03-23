4f5b5d92e1239d9f7ab9bac6a87982faac20d389
Ribeiro <joao.ribeiro17@outlook.com>
2023-03-23 10:26:17 -0300

    sobre for - estrutura de repetição com variavel de controle

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+//2 Métodos diferentes com a mesma funcionalidade
+/*P1)
+var c = 1
+while (c <=10) {
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+//P2)
+console.log('Começo')
+for (var c = 1; c <= 5; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim')
