d1e690e943f344d33b5b7afb4d575b3640ebe2f4
Ribeiro <joao.ribeiro17@outlook.com>
2023-03-22 22:54:47 -0300

    Estruturas de repetições (while, do)

---
+++ aula013/ambiente.js
@@ -0,0 +1,14 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while ( contador <= 6) 
+
+//Outro modo V
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
